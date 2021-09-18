b8010c486b97f9a4d873a4396e827e0b74310631
Emmvish <76655197+Emmvish@users.noreply.github.com>
2021-09-18 18:30:04 +0530

    Added Environment Variable for MongoDB Host

--- order/db/mongoose.js
+++ order/db/mongoose.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 
 const dbPort = process.env.MONGODB_PORT || 27017;
-const dbUrl = "mongodb://127.0.0.1:" + dbPort + "/order";
+const dbHost = process.env.MONGODB_HOST || "mongodb://127.0.0.1"
+const dbUrl = dbHost + ":" + dbPort + "/order";
 
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
